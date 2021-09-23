fb239a0041fb9136f6d8fedf9a043eacfd0e420d
Ekaterina Lyamina <svetlanka65@inbox.ru>
2021-09-23 15:08:37 +0300

    1 version

---
+++ src/perevjortish/Perevjortish.java
@@ -0,0 +1,16 @@
+package perevjortish;
+
+import java.util.Scanner;
+
+
+public class Perevjortish {
+
+
+    public static void main(String[] args) {
+        System.out.print("Введите строку: ");
+        Scanner scanner=new Scanner(System.in);
+        String str = scanner.nextLine();
+
+    }
+    
+}
